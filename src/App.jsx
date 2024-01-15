326b2b947cfe2b586d91cd89912b5d2b6f4e8412
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-01-15 15:26:30 +0800

    added theme context

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext("light");
 
 const App = () => {
+  const [theme, setTheme] = useState(null);
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <BrowserRouter>
+          <Navbar toggleTheme={toggleTheme} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
