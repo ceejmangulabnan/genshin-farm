cd07dd2707b957b15251221fb97b8ec1ccb3946c
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-01-15 16:06:06 +0800

    fix: all char icons are rendered

--- src/components/CharIcon.jsx
+++ src/components/CharIcon.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 
 // Display Chars that can farm on the current day separated by region
-//
-// Generate Char Icon URL and return icon
+
 const CharIcon = ({ char }) => {
   const getCharIconUrl = () => {
     // Generate CharIcon Url
-    // https://genshin.jmp.blue/characters/${char}/icon-big
     let charIconUrl = `https://genshin.jmp.blue/characters/${char}/icon`;
     if (charIconUrl.includes(`traveler`)) {
       // return charIconUrl.concat(`-lumine`);
     }
+
+    // Specify which characters I want to use 'icon-big'
+    if (charIconUrl.includes("lyney")) {
+      return `https://genshin.jmp.blue/characters/${char}/icon-big
+`;
+    }
     return charIconUrl;
   };
+  const fallbackCharIconUrl = `https://genshin.jmp.blue/characters/${char}/icon-big`;
 
   // TODO: DNE images return 404 error. Handle by trying other image resources.
   return (
     <div className="max-w-[6rem] bg-indigo-600 m-1 rounded-xl overflow-hidden">
       <img
         src={getCharIconUrl()}
+        onError={(e) => {
+          e.target.onError = null;
+          e.target.src = fallbackCharIconUrl;
+        }}
         className="object-cover object-position-top w-[6rem] h-[6rem]"
       />
     </div>
